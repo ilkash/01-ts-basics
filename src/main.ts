b6ee643aa82d7cb78d0f452f0a440c326da34301
ilkash <126604951+ilkash@users.noreply.github.com>
2025-07-18 13:52:08 +0300

    to do all tasks

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import "./style.css";
-import "./task-1";
-import "./task-2";
-import "./task-3";
-import "./task-4";
+// import "./style.css";
+// import "./task-1";
+// import "./task-2";
+// import "./task-3";
+// import "./task-4";
+import "./task-5";
